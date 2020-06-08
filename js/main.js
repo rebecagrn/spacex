5d9e9157399a1d9eab74806e752fab53c1fdf17a
Rebeca Pacheco <rbkgarner@gmail.com>
2020-06-08 12:35:38 -0300

    carousel fixed

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
 //'use strict'
 
 // Menu
-var el = document.getElementById('menu'); // target
-var numPx = '250';
+var el = document.getElementById("menu"); // target
+var numPx = "250";
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY > numPx) {
-    	el.classList.add('menu-scroll');
-    } else {
-      el.classList.remove('menu-scroll');
-    }
+window.addEventListener("scroll", function () {
+  if (window.scrollY > numPx) {
+    el.classList.add("menu-scroll");
+  } else {
+    el.classList.remove("menu-scroll");
+  }
 });
 
 // blog carousel
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  responsiveClass:true,
-  responsive:{
-      0:{
-          items:1,
-          nav:true
-      },
-      600:{
-          items:3,
-          nav:false
-      },
-      1000:{
-          items:3,
-          nav:true,
-          loop:false
-      }
-  }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  nav: true,
+  navText: [
+    '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+    '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+    },
+    600: {
+      items: 3,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: true,
+      loop: false,
+    },
+  },
+});
