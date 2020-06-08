ae63ae4d5b4a1b818aec07e2b9b43e3422c436dd
Rebeca Pacheco <rbkgarner@gmail.com>
2020-06-08 10:38:09 -0300

    div popular & blog added, fix mobile and js scripts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 //'use strict'
+
+var el = document.getElementById('menu'); // target
+var numPx = '250';
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > numPx) {
+    	el.classList.add('menu-scroll');
+    } else {
+      el.classList.remove('menu-scroll');
+    }
+});
